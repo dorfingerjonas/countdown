5c87212a22b672bbf40350882e40d23efb84de1d
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-08-27 12:21:31 +0200

    removed mistake

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     
     setInterval(() => {
         alert(window.innerWidth);
-    ), 10000};
+    }, 10000);
     
     let interval = setInterval(() => {
         let months, weeks, days, hours, minutes, seconds;
