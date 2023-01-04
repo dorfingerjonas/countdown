084060383bc82b7241d985631a95fbcc15a5dfcc
Jonas Dorfinger <contact@dorfingerjonas.at>
2023-01-04 21:20:43 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    const finalDate = new Date(2022, 11, 20, 18, 5).getTime();
+    const finalDate = new Date(2023, 3, 1, 18, 0).getTime();
 
     const interval = setInterval(() => {
         const timeData = {};
