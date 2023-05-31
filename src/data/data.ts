09bc49ff34b3200a76b04bc1c5642e3d1ca98f66
Jonas Dorfinger <contact@dorfingerjonas.at>
2023-05-31 17:04:23 +0200

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,7 @@
 import { CountdownConfig } from '../app/app.component';
 
 export const data: CountdownConfig = {
-  'zivi': new Date(2023, 4, 31, 23, 59, 59, 999),
+  'zivi': new Date(2023, 4, 31, 18),
   'geburtstag': new Date(2023, 5, 3),
   'homecoming': new Date(2023, 6, 14, 22, 40),
   '': new Date(2023, 5, 10, 11)
