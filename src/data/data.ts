d0650d1f32721bf45e2e318784701f0c508d17a4
Jonas Dorfinger <contact@dorfingerjonas.at>
2023-07-11 21:28:25 +0200

    remove expired dates

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,8 +1,5 @@
 import { CountdownConfig } from '../app/app.component';
 
 export const data: CountdownConfig = {
-  'zivi': new Date(2023, 4, 31, 18),
-  'geburtstag': new Date(2023, 5, 3),
-  'homecoming': new Date(2023, 6, 14, 22, 40),
-  '': new Date(2023, 5, 10, 11)
+  'default': new Date(2023, 6, 14, 22, 40),
 };
