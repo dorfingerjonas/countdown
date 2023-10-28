ca7462a72204834ceb4467b4fdd2f6c13267886a
Jonas Dorfinger <contact@dorfingerjonas.at>
2023-10-28 14:45:23 -0400

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,5 @@
 import { CountdownConfig } from '../app/app.component';
 
 export const data: CountdownConfig = {
-  'default': new Date(2023, 10, 5, 17, 0),
+  '': new Date(2023, 10, 5, 17, 0),
 };
