ca0607a3a49fbf8c471b928be1d335f04195590f
Jonas Dorfinger <contact@dorfingerjonas.at>
2026-02-13 14:26:10 +0100

    update data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,6 @@
 import { CountdownConfig } from '../app/app.component';
 
 export const data: CountdownConfig = {
-  'Jönköping': new Date(2026, 2, 28, 16, 45),
+  '': new Date(2026, 2, 28, 16, 45),
+  'Birthday': new Date(2026, 5, 3, 0, 0),
 };
